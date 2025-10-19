2d678a84f67c971327c64ab794d5310c8b3bab91
Ahkhan1815 <ahkhan1815@gmail.com>
2025-10-18 21:56:12 -0500

    updated pipeline + health route

--- server/app.py
+++ server/app.py
@@ -13,6 +13,10 @@ CORS(app)
 
 
 
+@app.route('/health', methods=['GET'])
+def health_check():
+    return {'status': 'healthy'}, 200
+
 @app.route('/generateRecipe', methods=['POST'])
 def generateRecipe():
     data = request.get_json()
